0895f956bda9cc71608fd34f10ebdc32f8f5ddc7
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-07-02 14:31:10 +0300

    Database Preparation: add  User Entities

---
+++ realWorldBE/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace realWorldBE.Entities
+{
+    public class User
+    {
+        [Key]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+
+        public string Image { get; set; }
+        public string Bio { get; set; }
+
+
+
+    }
+}
