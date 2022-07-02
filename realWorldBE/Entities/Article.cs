1f4c9cf597e75a8a4ee4e644203381f4425400f3
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-07-02 14:49:02 +0300

    Database Preparation: add  Article Entities

---
+++ realWorldBE/Entities/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace realWorldBE.Entities
+{
+    public class Article
+    {
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public ICollection<String> Tag { get; set; }
+            = new List<String>();
+
+
+    }
+}
